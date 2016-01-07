84754e84024a2207893e5f827cb78b9a3191ce28
jaychow <jay.chow@katanaconnect.com>
2016-01-06 19:54:03 -0800

    update triangle position

--- httpdocs/wp-content/themes/wp_theme_mievent/site/js/canvas.js
+++ httpdocs/wp-content/themes/wp_theme_mievent/site/js/canvas.js
@@ -96,10 +96,10 @@ jQuery(window).load(function() {
         })();
 
         function init() {
-            _this.pos.x = width*0.75;
-            _this.pos.y = height*0.75-20;
-            _this.coords[0].x = -10+Math.random()*40;
-            _this.coords[0].y = -10+Math.random()*40;
+            _this.pos.x = width*0.5;
+            _this.pos.y = height*0.5-20;
+            _this.coords[0].x = -10+Math.random()*100;
+            _this.coords[0].y = -10+Math.random()*100;
             _this.coords[1].x = -10+Math.random()*40;
             _this.coords[1].y = -10+Math.random()*40;
             _this.coords[2].x = -10+Math.random()*40;
