6cc733345f28a5cfc8b16c65aeb8d428dc0ccdc1
patrick <patimpey@gmail.com>
2017-01-11 17:30:24 -0800

    fixing logo size

--- httpdocs/wp-content/themes/wp_theme_mievent/mtheme/shortcodes.php
+++ httpdocs/wp-content/themes/wp_theme_mievent/mtheme/shortcodes.php
@@ -3851,7 +3851,7 @@ function hero_background($atts, $content=null) {
 						$contentOut .= '<a href="https://www.facebook.com/events/476822869190142/" target="_blank"><i class="fa fa-calendar fa-2x"></i></a> <a href="http://www.facebook.com/ClubVIPSD" target="_blank"><i class="fa fa-facebook-official fa-2x"></i></a>';
 					$contentOut.='</div>';
 					$contentOut.='<div class="text-center">';
-						$contentOut.='<img class="img-responsive" src="'.CHILD_URI.'img/mardi-gras-2017.jpg" alt="San Diego Mardi Gras Block Party, Parade & Celebration Presented by Tickled Pink Production"/>';
+						$contentOut.='<img style="max-height:200px; class="img-responsive" src="'.CHILD_URI.'img/mardi-gras-2017.jpg" alt="San Diego Mardi Gras Block Party, Parade & Celebration Presented by Tickled Pink Production"/>';
 					$contentOut.='</div>';
 					$contentOut.='<div class="event_time">';
 						$contentOut.='Saturday, February 25, 2017';
@@ -3865,7 +3865,7 @@ function hero_background($atts, $content=null) {
 					$contentOut .= '<a href="https://www.facebook.com/events/1103032579731678/" target="_blank"><i class="fa fa-calendar fa-2x"></i></a> <a href="http://www.facebook.com/GaslampMardiGras" target="_blank"><i class="fa fa-facebook-official fa-2x"></i></a>';
 				$contentOut.='</div>';
 				$contentOut.='<div class="text-center">';
-					$contentOut.='<img class="img-responsive" src="'.CHILD_URI.'img/fat-tuesday-2017.jpg" alt="10th Annual San Diego Fat Tuesday Party Gras Presented by Club VIP and Gaslamp’s Top Nightclubs, Restaurants & Bars"/>';
+					$contentOut.='<img style="max-height:200px;" class="img-responsive" src="'.CHILD_URI.'img/fat-tuesday-2017.jpg" alt="10th Annual San Diego Fat Tuesday Party Gras Presented by Club VIP and Gaslamp’s Top Nightclubs, Restaurants & Bars"/>';
 				$contentOut.='</div>';
 				$contentOut.='<div class="event_time">';
 					$contentOut.='Fat Tuesday, February 28, 2017';
