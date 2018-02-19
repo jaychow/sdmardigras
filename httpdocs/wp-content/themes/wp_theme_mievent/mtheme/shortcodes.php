fd1ab8d74363896fe698998df26a73a64289539d
patrick <patimpey@gmail.com>
2018-02-19 13:51:57 -0800

    updating mardi gras dates

--- httpdocs/wp-content/themes/wp_theme_mievent/mtheme/shortcodes.php
+++ httpdocs/wp-content/themes/wp_theme_mievent/mtheme/shortcodes.php
@@ -3857,11 +3857,6 @@ function hero_background($atts, $content=null) {
 					$contentOut.='</div>';
 					$contentOut.='<div class="event_time">';
 					$contentOut.= $mardiGras;
-					$contentOut.='Gaslamp Mardi Gras Party Hop!';
-					$contentOut.='<br />';
-					$contentOut.='March 1 & 2, 2019';
-					$contentOut.='<br />';
-					$contentOut.='6pm - 2am Begins at Vin De Syrah';
 					$contentOut.='</div>';
 					$contentOut.='<div>';
 						$contentOut.='<a class="first-button button button-large radius-none btn-effect wow  fadeIn" href="https://nightout.com/events/partygras/tickets" target="_blank"">Buy Tickets</a>';
